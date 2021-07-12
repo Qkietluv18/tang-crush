069d37b518626f5d8d3872b4ed0becb781aa1c2e
Qkietluv18 <87316235+Qkietluv18@users.noreply.github.com>
2021-07-12 21:48:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,24 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    introTitle: 'thư ơi !',
+    introDesc: '3 năm cấp 3 , lúc đầu chỉ xem 
+    Thư là bạn thôi , một người bạn khá hợp 
+    nói chuyện nhưng càng tiếp xúc càng thấy
+    thích... Thích thầm , thế là cũng quyết 
+    định nói ra , nói xong thì xác định 
+    mất tình bạn lúc trước nhưng thà rằng
+    nói ra thấy lòng nhẹ hơn là giữ kín
+    tuy không còn nói chuyện như trước nhưng
+    tui vẫn quang tâm và để ý`,
+    btnIntro: 'hihi😊',
+    title: 'Liệu thư có tình cảm vs tui không ',
+    desc: 'cho tui cơ hội nhaaaaa 🥰 ',
+    btnYes: ' cũng có đấy <33',
+    btnNo: 'Không :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại thích anh <3',
+    btnReply: 'Gửi iu thw <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'ỏ ỏ ỏ  🥰. Yêu nhiều nhiều 😘😘',
+    messDesc: 'giờ thì nhắn tin nhá <3.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/chauquockiet1811
 }
